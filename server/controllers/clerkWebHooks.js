c491a96eed6765cfab5e5228cd30db28161c0fd3
dhairya-shah05 <dhairyashah1769@gmail.com>
2026-06-27 17:32:07 +0530

    chal ja bhai

--- server/controllers/clerkWebHooks.js
+++ server/controllers/clerkWebHooks.js
@@ -6,9 +6,9 @@ const clerkWebHooks = async (req, res) => {
         // Create a Svix instance with clerk webhook secret
         const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
         const headers = {
-            "svix-id": req.header["sivx-id"],
-            "svix-timestamp": req.header["svix-timestamp"],
-            "svix-signature": req.header["svix-signature"],
+            "svix-id": req.headers["svix-id"],
+            "svix-timestamp": req.headers["svix-timestamp"],
+            "svix-signature": req.headers["svix-signature"],
         };
 
         await whook.verify(JSON.stringify(req.body), headers)

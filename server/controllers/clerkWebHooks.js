9551239682de37163a7d8bd25d49aecf088cde5e
dhairya-shah05 <dhairyashah1769@gmail.com>
2026-06-27 13:51:10 +0530

    Initial Commit

---
+++ server/controllers/clerkWebHooks.js
@@ -0,0 +1,47 @@
+import User from "../models/User.js";
+import { Webhook } from "svix";
+
+const clerkWebHooks = async (requestAnimationFrame, res) => {
+    try {
+        // Create a Svix instance with clerk webhook secret
+        const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
+        const headers = {
+            "svix-id": req.header["sivx-id"],
+            "svix-timestamp": req.header["svix-timestamp"],
+            "svix-signature": req.header["svix-signature"],
+        };
+
+        await whook.verify(JSON.stringify(req.body), headers)
+
+        const { data, type } = req.body
+        const userData = {
+            _id: data.id,
+            email: data.email_addresses[0].email_address,
+            username: data.first_name + " " + data.last_name,
+            image: data.image_url,
+        }
+
+        switch (type) {
+            case "user.created": {
+                await User.create(userData);
+                break;
+            }
+            case "user.updated": {
+                await User.findByIdAndUpdate(data.id, userData);
+                break;
+            }
+            case "user.deleted": {
+                await User.findByIdAndDelete(data.id);
+                break;
+            }
+            default: break;
+        }
+
+        res.json({ success: true, message: "Webhook Recieved" })
+    } catch (error) {
+        console.log(error.message);
+        res.json({ success: "false", message: error.message });
+    }
+}
+
+export default clerkWebHooks;

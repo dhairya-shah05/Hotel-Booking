28d824b5a7b6836789c41381fc977deab86888a2
dhairya-shah05 <dhairyashah1769@gmail.com>
2026-06-28 13:22:46 +0530

    Update bookingRoutes.js

--- server/routes/bookingRoutes.js
+++ server/routes/bookingRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { checkAvailabilityAPI, createBooking, getHotelBookings, getUserBookings } from "../controllers/bookingController.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 const bookingRouter = express.Router();
 
